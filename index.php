c1ca7c0f47d5beb8e97b75d074d5298b3db26a2b
Daniel Urra <danielurra75@gmail.com>
2022-06-30 21:57:20 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basics</title>
+</head>
+
+<body>
+    <?php
+        echo "<h2>Hello world!</h2>";
+        $name = "Albert Einstein";
+        echo "<h1>My name is: $name</h1>";
+        echo "<h2>It's a pleasure to meet you</h2>";
+    ?>
+</body>
+</html>
